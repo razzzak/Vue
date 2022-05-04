aa688ba19c95ff84cfd6c1159f95a97b47c726fe
razak <ipoxe@mail.ru>
2022-05-04 17:56:56 +0300

    добавлено дз по уроку №2

--- src/components/CA.vue
+++ src/components/CA.vue
@@ -1,137 +1,162 @@
 <template>
-  <div class="calc">
-    <div class="calc-body">
-      <input
-        type="text"
-        class="calc-input"
-        placeholder="Введите первое число"
-        v-model.number="operand1"
-      />
-      <input
-        type="text"
-        class="calc-input"
-        placeholder="Введите второе число"
-        v-model.number="operand2"
-      />
-      <div class="btn-wrapper">
-        <button class="btn-operation btn" @click="add()">+</button>
-        <button class="btn-operation btn" @click="subtract()">-</button>
-        <button class="btn-operation btn" @click="multiply()">*</button>
-        <button class="btn-operation btn" @click="divide()">/</button>
-        <button class="btn-operation btn" @click="exponet()">**</button>
-        <button class="btn-operation btn" @click="integerDivide()">%</button>
-      </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <hr>
+
+    <input type="number" v-model.number.lazy="operand1" ref="input1" />
+    <input type="number"  v-model.number.lazy="operand2" ref="input2" />
+    <p v-if="result == Infinity">На ноль делить нельзя</p>
+    <p v-else>= {{ result }}</p>
+    <br>
+    <button class ="btn__calc" @click="sum()">+</button>
+    <button class ="btn__calc" @click="substract()">-</button>
+    <button class ="btn__calc" @click="multiply()">*</button>
+    <button class ="btn__calc" @click="degree()">**</button>
+    <button class ="btn__calc" @click="divide()">/</button>
+    <button class ="btn__calc" @click="remainder()">%</button>
+    <button class ="btn__calc" @click="removebutton()">R</button>
+
+    <hr>
+
+    <div class="test">
+          <input type="checkbox" id="checkbox" v-model="checked" />Отобразить клавиатуру
+          <div v-if="checked == true">
+            <div class="btn__wrp">
+          <button class="btn" @click="input(num)" v-for="num in numbers" :key="num">{{ num }}</button>
+          </div>
+        </div>
     </div>
-    <span class="result"> Результат: {{ result }}</span>
+    
+    <input @click="foc1()" type="radio" name="test" value="operand1" v-model="picked" />Операнд 1
+    <input @click="foc2()" type="radio" name="test" value="operand2" v-model="picked" />Операнд 2
   </div>
 </template>
 
+
+
 <script>
 export default {
-  name: "CalcApp",
+  name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: String
   },
   data() {
     return {
-      result: "",
-      operand1: "",
-      operand2: "",
+      result: '',
+      operand1: '',
+      operand2: '',
+      checked: true,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operands: ["+", "-", "/", "*","**", "%","<--"],
+      picked:[],
     };
   },
   methods: {
-    clearInputs() {
-      this.operand1 = "";
-      this.operand2 = "";
+    input(test) {
+      if(this.picked === "operand1"){
+        this.operand1 += parseInt(test)
+      }else{
+        this.operand2 += parseInt(test)
+      }
     },
-    add() {
-      this.result = this.operand1 + this.operand2;
-      this.clearInputs();
+    removebutton() {
+      if(this.operand1.length>1) {
+        this.operand1 = this.operand1.slice(0,this.operand1.length-1)
+      }else{
+        this.operand1=''
+      }
+      if(this.operand2.length>1) {
+        this.operand2 = this.operand2.slice(0,this.operand2.length-1)
+      }else{
+        this.operand2=''
+      }
     },
-    subtract() {
-      this.result = this.operand1 - this.operand2;
-      this.clearInputs();
+    calculate(operation) {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.sum();
+          break;
+        case "-":
+          this.substract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "**":
+          this.degree();
+          break;
+        case "/":
+          this.divide();
+          break;
+        case "%":
+          this.remainder();
+          break;
+        case "<--":
+          this.removebutton();
+          break;
+      }
     },
-    multiply() {
-      this.result = this.operand1 * this.operand2;
-      this.clearInputs();
+    
+    foc1() {
+        this.$refs.input1.focus();
+    },
+    foc2() {
+        this.$refs.input2.focus();
+    },
+    sum(){
+      this.result = Number(this.operand1) + Number(this.operand2)
+    },
+    substract() {
+      this.result = this.operand1-this.operand2
     },
     divide() {
-      if (this.operand2 === 0) {
-        alert("На ноль делить нельзя");
-      } else {
-        this.result = this.operand1 / this.operand2;
-      }
-      this.clearInputs();
+      this.result = this.operand1 / this.operand2
     },
-    exponet() {
-      this.result = this.operand1 ** this.operand2;
-      this.clearInputs();
+    multiply() {
+      this.result = this.operand1 * this.operand2
     },
-    integerDivide() {
-      this.result = Math.floor(this.operand1 / this.operand2);
-      this.clearInputs();
+    degree() {
+      this.result = this.operand1 ** this.operand2
     },
+    remainder(){
+      this.result = this.operand1 % this.operand2
+  },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.calc {
-  margin: 0 auto;
-  max-width: 300px;
+h3 {
+  margin: 40px 0 0;
 }
-.calc-body {
-  margin-inline: auto;
-  max-width: 300px;
-  width: 100%;
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.btn__wrp {
   display: flex;
-  flex-direction: column;
-}
-.calc-input {
-  padding: 8px;
-  text-align: center;
-}
-.result {
-  display: block;
-  font-size: 16px;
-  text-transform: uppercase;
-  text-align: left;
-}
-.btn-wrapper {
-  display: flex;
+  justify-content: center;
   flex-wrap: wrap;
-  width: 100%;
-  margin-bottom: 18px;
+  width: 150px;
+  margin: 0 auto;
 }
 .btn {
-  height: 50px;
   width: 50px;
-  font-size: 16px;
-  color: #fff;
-  border: 0;
-  cursor: pointer;
-  font-weight: 700;
+  height: 50px;
 }
-.btn-num {
-  background-color: #42b983;
-}
-.btn-operation {
-  background-color: rgb(180, 165, 159);
-}
-.btn-num:hover {
-  background-color: rgb(159, 184, 159);
-}
-.btn-operation:hover {
-  background-color: rgb(0, 0, 0);
-}
-.btn-accent {
-  width: 50%;
-  text-transform: uppercase;
-  background-color: rgb(0, 0, 0);
-}
-.btn-accent:hover {
-  background-color: #a88989;
+.btn__calc {
+  width: 30px;
+  height: 30px;
+  margin: 5px;
 }
 </style>
