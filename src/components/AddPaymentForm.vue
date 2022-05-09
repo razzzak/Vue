7c7507ca0ce362c723c004c047e71e37dcf0fb07
razak <ipoxe@mail.ru>
2022-05-09 17:30:18 +0300

    домашнее задание 3 урок

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="form-wrapper">
+    <button class="btn__add" @click = "visible=!visible">+ADD NEW COST</button>
+
+    <div v-show="visible">
+    <!--<input v-model="date"  placeholder="date"/>-->
+    <input v-model="category" placeholder="category"/>
+    <input v-model="value" placeholder="value"/>
+    <button @click="onClickSave">Save</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddPaymentForm",
+  data() {
+    return {
+      /*date: "",*/
+      category: "",
+      value: "",
+      visible:false,
+    }
+  },
+  computed: {
+    getCurrentDate(){
+      const today = new Date()
+      const shortDate = new Intl.DateTimeFormat("ru", {dateStyle: "short"}).format(today);
+      return shortDate
+    }
+  },
+  methods: {
+    onClickSave(){
+      const data = {
+        date: this.date || this.getCurrentDate,
+        category: this.category,
+        value: this.value
+      }
+      this.$emit('addNewPayment', data)
+      console.log(data);
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .btn__add {
+    background-color:#009f95;
+    color: white;
+    border: none;
+    padding: 8px;
+    margin: 20px 0 20px 0;
+  }
+  
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,130 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <hr>
+
+    <input type="number" v-model.number.lazy="operand1" ref="input1" />
+    <input type="number"  v-model.number.lazy="operand2" ref="input2" />
+    <p v-if="result == Infinity">На ноль делить нельзя</p>
+    <p v-else>= {{ result }}</p>
+    <br>
+    <button class ="btn__calc" @click="sum()">+</button>
+    <button class ="btn__calc" @click="substract()">-</button>
+    <button class ="btn__calc" @click="multiply()">*</button>
+    <button class ="btn__calc" @click="degree()">**</button>
+    <button class ="btn__calc" @click="divide()">/</button>
+    <button class ="btn__calc" @click="remainder()">%</button>
+    <button class ="btn__calc" @click="removebutton()">R</button>
+
+    <hr>
+
+    <div class="test">
+          <input type="checkbox" id="checkbox" v-model="checked" />Отобразить клавиатуру
+          <div v-if="checked == true">
+            <div class="btn__wrp">
+          <button class="btn" @click="input(num)" v-for="num in numbers" :key="num">{{ num }}</button>
+          </div>
+        </div>
+    </div>
+    
+    <input @click="foc1()" type="radio" name="test" value="operand1" v-model="picked" />Операнд 1
+    <input @click="foc2()" type="radio" name="test" value="operand2" v-model="picked" />Операнд 2
   </div>
 </template>
+
+
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
-}
+  },
+  data() {
+    return {
+      result: '',
+      operand1: '',
+      operand2: '',
+      checked: true,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operands: ["+", "-", "/", "*","**", "%","<--"],
+      picked:[],
+    };
+  },
+  methods: {
+    input(test) {
+      if(this.picked === "operand1"){
+        this.operand1 += parseInt(test)
+      }else{
+        this.operand2 += parseInt(test)
+      }
+    },
+    removebutton() {
+      if(this.operand1.length>1) {
+        this.operand1 = this.operand1.slice(0,this.operand1.length-1)
+      }else{
+        this.operand1=''
+      }
+      if(this.operand2.length>1) {
+        this.operand2 = this.operand2.slice(0,this.operand2.length-1)
+      }else{
+        this.operand2=''
+      }
+    },
+    calculate(operation) {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.sum();
+          break;
+        case "-":
+          this.substract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "**":
+          this.degree();
+          break;
+        case "/":
+          this.divide();
+          break;
+        case "%":
+          this.remainder();
+          break;
+        case "<--":
+          this.removebutton();
+          break;
+      }
+    },
+    
+    foc1() {
+        this.$refs.input1.focus();
+    },
+    foc2() {
+        this.$refs.input2.focus();
+    },
+    sum(){
+      this.result = Number(this.operand1) + Number(this.operand2)
+    },
+    substract() {
+      this.result = this.operand1-this.operand2
+    },
+    divide() {
+      this.result = this.operand1 / this.operand2
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2
+    },
+    degree() {
+      this.result = this.operand1 ** this.operand2
+    },
+    remainder(){
+      this.result = this.operand1 % this.operand2
+  },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -56,5 +142,21 @@ li {
 }
 a {
   color: #42b983;
+}
+.btn__wrp {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100px;
+  margin: 0 auto;
+}
+.btn {
+  width: 50px;
+  height: 50px;
+}
+.btn__calc {
+  width: 30px;
+  height: 30px;
+  margin: 3px;
 }
 </style>
